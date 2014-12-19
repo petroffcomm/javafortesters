860ee334c05843e0d70a194483a49a8d56aff5d7
petroffcomm <petroffcomm@gmail.com>
2014-12-19 16:25:29 +0200

    Home Task 5

--- addressbook-selenium-tests/src/com/example/tests/GroupCreationTests.java
+++ addressbook-selenium-tests/src/com/example/tests/GroupCreationTests.java
@@ -1,32 +1,40 @@
 package com.example.tests;
 
+import static org.testng.Assert.assertEquals;
+
+import java.util.Collections;
+import java.util.List;
+
 import org.testng.annotations.Test;
 
 public class GroupCreationTests extends TestBase{
-  @Test
-  public void testNonEmptyGroupCreation() throws Exception {
-    GroupData group = new GroupData();
+  
+  @Test(dataProvider = "randomValidGroupGenerator")
+  public void testGroupCreationWithValidData(GroupData group) throws Exception {
+    /*GroupData group = new GroupData();
     group.name = "group name 1";
     group.header = "header 1";
-    group.footer = "footer 1";
+    group.footer = "footer 1";*/
 
     app.getNavigationHelper().openMainPage();
     app.getNavigationHelper().gotoGroupsPage();
+    
+    //save old state
+    List<GroupData> oldList= app.getGroupHelper().getGroups();    
+    
+    //actions
 	app.getGroupHelper().initGroupCreating();
 	app.getGroupHelper().fillGroupForm(group);
     app.getGroupHelper().submitGroupCreation();
     app.getGroupHelper().returnToGroupsPage();
+    
+    //save new state
+    List<GroupData> newList= app.getGroupHelper().getGroups();
+    
+    //compare states
+    oldList.add(group);
+    Collections.sort(oldList);
+    assertEquals(newList, oldList);
   }
   
-  @Test
-  public void testEmptyGroupCreation() throws Exception {
-    GroupData group = new GroupData();
-    
-    app.getNavigationHelper().openMainPage();
-    app.getNavigationHelper().gotoGroupsPage();
-	app.getGroupHelper().initGroupCreating();
-	app.getGroupHelper().fillGroupForm(group);
-    app.getGroupHelper().submitGroupCreation();
-    app.getGroupHelper().returnToGroupsPage();
-  }
 }
